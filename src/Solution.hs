1e4a0f3a556d014f0cb20909c0867123f2223486
Jeff Pratt <jpratt@ancestry.com>
2019-03-07 10:54:38 -0700

    Use Control.Arrow instead of Data.Bifunctor

--- src/Solution.hs
+++ src/Solution.hs
@@ -1,8 +1,8 @@
 module Solution (Keypad, totalDistance) where
 
 import Control.Applicative (liftA2)
+import Control.Arrow ((***))
 import Control.Monad (join, sequence)
-import Data.Bifunctor (bimap)
 import Data.Either.Utils (maybeToEither)
 import Data.List (elemIndex)
 
@@ -16,9 +16,8 @@ totalDistance s keypad = sum <$> sequence dists
         locs = map (location keypad) s
 
 distance :: Location -> Location -> Int
-distance left right = uncurry max $ pairAbs $ diff left right
+distance left right = uncurry max $ abs *** abs $ diff left right
   where diff a b = (fst a - fst b, snd a - snd b)
-        pairAbs = join bimap abs
 
 location :: Keypad -> Char -> Either String Location
 location keypad c = coords <$> maybeToEither ("Invalid character " ++ show c) (elemIndex c keypad)
